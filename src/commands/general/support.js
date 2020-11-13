2feebba4f6f991068ad0442beaabb2f392865c07
AllenKurian <44091241+CosmoHomeLLC@users.noreply.github.com>
2020-11-12 18:03:00 -0600

    New Files

--- src/commands/general/support.js
+++ src/commands/general/support.js
@@ -10,8 +10,8 @@ class Support extends Command {
 
   async run(msg, args) { // eslint-disable-line no-unused-vars
     return msg.send(this.client.embed()
-      .setTitle("Join Miyako Lounge")
-      .setDescription("If you need help with setting me up on your server or just want to hangout, join Miyako Lounge.\nYou also get an oppurtunity to become a Premium user which can only be obtained through a role in our server.\nYou will also recieve updates about the bot and much more!\n\n[Join Miyako Lounge](https://discord.gg/mDkMbEh)")
+      .setTitle("Join WaveForm Support")
+        .setDescription("Hello my friend, if you need help setting up, join WaveForm Support.\nYou also get an oppurtunity to become a WaveForm Pro user which can only be obtained through a role in our server.\nYou will also recieve updates about the bot and much more!\n\n[Join Miyako Lounge](https://discord.gg/fAM2dJtfx7)")
       .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL({ size: 64 })));
   }
 }
