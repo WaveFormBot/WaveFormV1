f88fc8153c71a4cc57d7d1d3f35e4043d38fc685
AllenKurian <44091241+CosmoHomeLLC@users.noreply.github.com>
2020-11-16 10:04:34 -0600

    Update achievement.js

--- src/commands/images/achievement.js
+++ src/commands/images/achievement.js
@@ -28,7 +28,7 @@ class Achievement extends Command {
         const attachment = new MessageAttachment(img, "achievement.png");
 
         const embed = new MessageEmbed()
-            .setTitle("Cool title")
+            .setColor('#7D98F9')
             .attachFiles(attachment)
             .setImage("attachment://achievement.png")
 
