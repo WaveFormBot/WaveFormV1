2f582bdc1b6d291ae364325cc6abea1e60ffadb5
AllenKurian <44091241+CosmoHomeLLC@users.noreply.github.com>
2020-11-16 09:56:34 -0600

    Update achievement.js

--- src/commands/images/achievement.js
+++ src/commands/images/achievement.js
@@ -1,34 +1,39 @@
 const Command = require("../../structures/Command.js");
-const { MessageAttachment } = require("discord.js");
-const { MessageEmbed } = require("discord.js")
+const { MessageAttachment, MessageEmbed } = require("discord.js");
 
 class Achievement extends Command {
-  constructor(...args) {
-    super(...args, {
-      description: "Achievement Get!",
-      cooldown: 3,
-      cost: 5,
-      usage: "achievement [@user] <text>"
-    });
-  }
+    constructor(...args) {
+        super(...args, {
+            description: "Achievement Get!",
+            cooldown: 3,
+            cost: 5,
+            usage: "achievement [@user] <text>"
+        });
+    }
 
-  async run(msg, [user, ...args]) {
-    user = await this.verifyUser(msg, user, true)
-      .catch(() => {
-        args.unshift(user);
-        return msg.author;
-      });
+    async run(msg, [user, ...args]) {
+        user = await this.verifyUser(msg, user, true)
+            .catch(() => {
+                args.unshift(user);
+                return msg.author;
+            });
 
-    if(!args.length) return msg.send("Hey Friend, You need to provide some text.");
+        if (!args.length) return msg.send("Hey Friend, You need to provide some text.");
 
-    const text = args.join(" ");
+        const text = args.join(" ");
 
-    if(text.length > 21) return msg.send("Sorry Friend, Text cannot be longer than 21 characters.");
+        if (text.length > 21) return msg.send("Sorry Friend, Text cannot be longer than 21 characters.");
 
-    const img = await this.client.img.achievement(user.displayAvatarURL({ size: 64, format: "png" }), text);
+        const img = await this.client.img.achievement(user.displayAvatarURL({ size: 64, format: "png" }), text);
+        const attachment = new MessageAttachment(img, "achievement.png");
 
-      return msg.send(new MessageAttachment(img, "achievement.png"));
-  }
+        const embed = new MessageEmbed()
+            .setTitle("Cool title")
+            .attachFiles(achievement)
+            .setImage("attachment://achievement.png")
+
+        return message.channel.send(embed)
+    }
 }
 
 module.exports = Achievement;
